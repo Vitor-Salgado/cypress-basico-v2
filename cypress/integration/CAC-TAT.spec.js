009eacca14a92dd355714a8afc947a61a51ee518
Vitor-Salgado <110491805+Vitor-Salgado@users.noreply.github.com>
2022-09-26 15:03:41 -0300

    Fim do curso

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -132,9 +132,47 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#privacy a').invoke('removeAttr' , 'target').click()
         cy.contains('Talking About').should('be.visible')
     })
-    it('testa a página da política de privavidade de forma independente',function(){
-        cy.visit('./src/privacy.html')
-        cy.contains('Talking About').should('be.visible')
-    })
+
+    Cypress._.times(3,function(){
+        it('testa a página da política de privavidade de forma independente',function(){
+            cy.visit('./src/privacy.html')
+            cy.contains('Talking About').should('be.visible')
+        })
+    })  
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      })
+      it('preenche a area de texto usando o comando invoke',function(){
+        const longText = Cypress._.repeat('1234567890',20)
+
+        cy.get('#open-text-area')
+        .invoke('val',longText)
+        .should('have.value',longText)
+      })
+      it.only('faz uma requisição HTTP',function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response){
+            const {status, statusText, body}= response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
+      })
+      it.only('encontrando o gato',function(){
+        cy.get('#cat').invoke('show').should('be.visible')
+      })
   })
   
